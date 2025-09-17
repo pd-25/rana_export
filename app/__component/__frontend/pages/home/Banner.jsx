c26e3a51fcf2f3ce91de8c0732eac4711146505d
Supriyo Kumar Dey <supriyokumardey@Supriyos-MacBook-Air.local>
2025-09-18 02:11:11 +0530

    ll

--- app/__component/__frontend/pages/home/Banner.jsx
+++ app/__component/__frontend/pages/home/Banner.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import styles from '@/app/__assets/style/component/banner.module.css'
 import Image from 'next/image'
 import hero1 from '@/app/__assets/images/hero1.png'
+import hero2 from '@/app/__assets/images/light-bg-2.png'
 
 const Banner = () => {
     return (
-        <section className={styles.hero}>
+        <>
+          <section className={styles.hero}>
             <div className={styles['hero-content']}>
                 <h1>EXPLORE MINDFULNESS THROUGH SACRED SOUND</h1>
                 <p>with Silent Mind Singing Bowls</p>
@@ -18,6 +20,10 @@ const Banner = () => {
                 <Image src={hero1} alt="Singing Bowl" />
             </div>
         </section>
+        <div className={styles.nextimage2}></div>
+        </>
+      
+
 
     )
 }
